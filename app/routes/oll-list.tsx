8c8d81814df36cef989c6956cf5fa8657cb74356
Devin Shoemaker <devinshoe@gmail.com>
2023-12-02 17:03:25 -0600

    feat: add OLL list (#6)

---
+++ app/routes/oll-list.tsx
@@ -0,0 +1,23 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+
+import AlgorithmCard from "~/components/AlgorithmCard";
+import { olls } from "~/lib/cases";
+import { requireUserId } from "~/session.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  await requireUserId(request);
+  return null;
+};
+
+export default function OllList() {
+  return (
+    <div
+      id="page-container"
+      className="w-full p-4 flex flex-col items-center max-w-sm gap-4"
+    >
+      {olls.map((oll) => (
+        <AlgorithmCard algorithmCase={oll} key={oll.id} />
+      ))}
+    </div>
+  );
+}
---
+++ e2e/oll-list.spec.ts
@@ -0,0 +1,40 @@
+import { faker } from "@faker-js/faker";
+import test, { expect } from "@playwright/test";
+
+import { createUser } from "./lib/create-user";
+import { cleanupUser } from "./lib/utils";
+
+test.describe("OLL List", () => {
+  let email = "";
+  let shouldDeleteUser = true;
+
+  test.beforeEach(() => {
+    email = `${faker.internet.userName()}@example.com`;
+  });
+
+  test.afterEach(async ({ page }) => {
+    if (shouldDeleteUser) {
+      await cleanupUser(email, page);
+    }
+
+    shouldDeleteUser = true;
+  });
+
+  test("should redirect if user is unauthenticated", async ({ page }) => {
+    shouldDeleteUser = false;
+    await page.goto("/oll-list");
+    await expect(page).toHaveURL("/login?redirectTo=%2Foll-list");
+  });
+
+  test("side menu should navigate to timer", async ({ page }) => {
+    await createUser(email, page);
+    await page.getByText(/OLL List/i).click();
+    await expect(page).toHaveURL("/oll-list");
+  });
+
+  test("should render OLLs", async ({ page }) => {
+    await createUser(email, page);
+    await page.goto("/oll-list");
+    await expect(page.getByTestId("algorithm-card")).toHaveCount(4);
+  });
+});
