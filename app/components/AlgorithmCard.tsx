6bbbfb889e1a27ed2ee199e7372db5172a20d949
Devin Shoemaker <devinshoe@gmail.com>
2023-12-02 16:19:16 -0600

    feat: add OLL list

---
+++ app/components/AlgorithmCard.tsx
@@ -0,0 +1,36 @@
+import { Case } from "../lib/cases";
+
+export default function AlgorithmCard({
+  algorithmCase,
+}: {
+  algorithmCase: Case;
+}) {
+  return (
+    <div
+      id="algorithm-card"
+      data-testid="algorithm-card"
+      className="rounded p-4 w-full bg-slate-700 shadow"
+    >
+      <div id="info-row" className="flex items-center justify-between">
+        <div className="flex flex-col">
+          <div className="flex items-center">
+            <label id="case-name" className="text-3xl pr-4">
+              {algorithmCase.name}
+            </label>
+            <label id="case-group">{algorithmCase.group}</label>
+          </div>
+          <label id="case-algorithm">{algorithmCase.algorithm}</label>
+        </div>
+
+        <img src={algorithmCase.image} alt="" width="100" height="100" />
+      </div>
+
+      <div id="status-buttons" className="flex justify-between pt-4">
+        {/* TODO turn this into a radio? */}
+        <button>Not Learned</button>
+        <button>Learning</button>
+        <button>Learned</button>
+      </div>
+    </div>
+  );
+}
---
+++ app/lib/cases.ts
@@ -0,0 +1,35 @@
+export interface Case {
+  id?: number;
+  name: string;
+  group: "Dot";
+  algorithm: string;
+  image: string;
+  status?: string;
+}
+
+export const olls: Case[] = [
+  {
+    name: "1",
+    group: "Dot",
+    algorithm: `R U2 R2 F R F' U2 R' F R F'`,
+    image: "/images/oll/1.png",
+  },
+  {
+    name: "2",
+    group: "Dot",
+    algorithm: `F R U R' U' F' f R U R' U' f'`,
+    image: "/images/oll/2.png",
+  },
+  {
+    name: "3",
+    group: "Dot",
+    algorithm: `r' R2 U R' U r U2 r' U M'`,
+    image: "/images/oll/3.png",
+  },
+  {
+    name: "4",
+    group: "Dot",
+    algorithm: `M U' r U2 r' U' R U' R' M'`,
+    image: "/images/oll/4.png",
+  },
+];
