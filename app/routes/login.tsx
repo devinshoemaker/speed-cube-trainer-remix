674ea2e10e02ad9e4cf5d050d0d53e8d4bc6bfd7
Devin Shoemaker <devinshoe@gmail.com>
2024-01-25 22:22:23 -0600

    chore: validate auth forms with zod

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -6,10 +6,11 @@ import type {
 import { json, redirect } from "@remix-run/node";
 import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import { useEffect, useRef } from "react";
+import { z } from "zod";
 
 import { verifyLogin } from "~/models/user.server";
 import { createUserSession, getUserId } from "~/session.server";
-import { safeRedirect, validateEmail } from "~/utils";
+import { safeRedirect } from "~/utils";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const userId = await getUserId(request);
@@ -19,47 +20,39 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
-  const email = formData.get("email");
-  const password = formData.get("password");
   const redirectTo = safeRedirect(formData.get("redirectTo"), "/");
   const remember = formData.get("remember");
 
-  if (!validateEmail(email)) {
-    return json(
-      { errors: { email: "Email is invalid", password: null } },
-      { status: 400 },
-    );
+  const parsedCredentials = z
+    .object({
+      email: z.string().email({ message: "Email is invalid" }),
+      password: z
+        .string()
+        .min(1, { message: "Password is required" })
+        .min(6, { message: "Password is too short" }),
+    })
+    .safeParse(Object.fromEntries(formData));
+
+  if (parsedCredentials.success) {
+    const { email, password } = parsedCredentials.data;
+    const user = await verifyLogin(email, password);
+
+    if (!user) {
+      return json(
+        { errors: { email: "Invalid email or password", password: null } },
+        { status: 400 },
+      );
+    }
+
+    return createUserSession({
+      redirectTo,
+      remember: remember === "on" ? true : false,
+      request,
+      userId: user.id,
+    });
+  } else {
+    return json({ errors: parsedCredentials.error.formErrors.fieldErrors });
   }
-
-  if (typeof password !== "string" || password.length === 0) {
-    return json(
-      { errors: { email: null, password: "Password is required" } },
-      { status: 400 },
-    );
-  }
-
-  if (password.length < 8) {
-    return json(
-      { errors: { email: null, password: "Password is too short" } },
-      { status: 400 },
-    );
-  }
-
-  const user = await verifyLogin(email, password);
-
-  if (!user) {
-    return json(
-      { errors: { email: "Invalid email or password", password: null } },
-      { status: 400 },
-    );
-  }
-
-  return createUserSession({
-    redirectTo,
-    remember: remember === "on" ? true : false,
-    request,
-    userId: user.id,
-  });
 };
 
 export const meta: MetaFunction = () => [{ title: "Login" }];
